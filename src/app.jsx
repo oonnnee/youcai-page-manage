e9b522f41d4e3a550772af34decb4e116be4c754
Mac os <macos@MacdeMacBook-Pro-2.local>
2018-05-24 13:43:52 +0800

    --

---
+++ src/app.jsx
@@ -0,0 +1,15 @@
+import {React} from 'react'
+
+class Component extends React.Component{
+    constructor(props){
+      super(props)
+    }
+
+    render(){
+        return (
+            <h1>hello,jsx</h1>
+        );
+    }
+}
+
+<Component/>
