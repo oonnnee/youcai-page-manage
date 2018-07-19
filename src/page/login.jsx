bcf1711869d1d707ff24bb9f3ba80aeb66e91610
oonnnee <1729711680@qq.com>
2018-07-20 00:04:55 +0800

    '-'

--- src/page/login.jsx
+++ src/page/login.jsx
@@ -12,8 +12,8 @@ class Login extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            id: 'admin',
-            pwd: 'admin'
+            id: '',
+            pwd: ''
         }
     }
 
